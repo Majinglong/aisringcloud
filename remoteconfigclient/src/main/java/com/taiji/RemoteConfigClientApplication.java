73fa7c673c93ac8111826c6827ec1ea938ba6362
majla <majla@mail.taiji.com.cn>
2019-10-30 14:22:49 +0800

    演示远程配置中心

---
+++ remoteconfigclient/src/main/java/com/taiji/RemoteConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.taiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class RemoteConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteConfigClientApplication.class,args  );
+    }
+}
