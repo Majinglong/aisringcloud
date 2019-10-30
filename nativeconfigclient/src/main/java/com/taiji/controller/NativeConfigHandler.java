d7736f03c30e5af4fd032fd5db5c56841f66bd1f
majla <majla@mail.taiji.com.cn>
2019-10-30 15:37:11 +0800

    服务监控演示

--- nativeconfigclient/src/main/java/com/taiji/controller/NativeConfigHandler.java
+++ nativeconfigclient/src/main/java/com/taiji/controller/NativeConfigHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class NativeConfigHandler {
     @Value("${server.port}")
     private String port;
-    @Value("{foo}")
+    @Value("${foo}")
     private String foo;
 
     @GetMapping("/index")
--- zipkinclient/src/main/java/com/taiji/controller/ZipkinHandler.java
+++ zipkinclient/src/main/java/com/taiji/controller/ZipkinHandler.java
@@ -1,21 +1,19 @@
 package com.taiji.controller;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/remote")
-public class RemoteConfigHandler {
+@RequestMapping("/zipkin")
+public class ZipkinHandler {
+
     @Value("${server.port}")
     private String port;
-    @Value("${foo}")
-    private String foo;
 
     @GetMapping("/index")
-    public  String index(){
-        return this.port+"--"+this.foo;
+    public String index(){
+        return  this.port;
     }
 }
